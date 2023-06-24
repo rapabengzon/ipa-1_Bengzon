528237edb5c0decdd291bb6e11d1a5fd05d20ae6
rapabengzon <135810149+rapabengzon@users.noreply.github.com>
2023-06-25 01:17:16 +0800

    Update file

--- ipa-1.py
+++ ipa-1.py
@@ -33,7 +33,7 @@ def savings(gross_pay, tax_rate, expenses):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    net_income = gross_pay-(tax_rate*gross_pay)-expenses
+    net_income = gross_pay-(float(tax_rate)*gross_pay)-expenses
     return int(net_income)
 
 def material_waste(total_material, material_units, num_jobs, job_consumption):
@@ -70,7 +70,7 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
     waste = total_material-(num_jobs*job_consumption)
-    print(waste, material_units)
+    print(str(waste), material_units)
 
 def interest(principal, rate, periods):
     '''Interest.
@@ -100,7 +100,7 @@ def interest(principal, rate, periods):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    simple_int = (principal*rate*periods)+principal
+    simple_int = (principal*float(rate)*periods)+principal
     return simple_int
 
 
